9fb82ebf1b87acef6b21c6e18711739198d95ac5
Shane Donnelly <shanejdonnelly@gmail.com>
2013-12-31 13:00:48 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    //make height of boot title text equal to image height
+    var $container = $('.boot-title');
+    $container.imagesLoaded().always( sizeBootTitle);
+
+    $(window).on('resize', sizeBootTitle);
+
+    function sizeBootTitle(){
+        var $titles = $('.boot-title');
+        $titles.each(function(){
+            var img_height = $(this).find('.span23').height();
+            $(this).find('.span13').height(img_height); 
+        });
+    }
+    
+    var swiper_width = $('.swiper-container').first().width();
+
+    $('.swiper-slide').width(swiper_width * 0.5);
+    
+    var mySwiper = $('.swiper-container').swiper({
+        //Your options here:
+        slidesPerView: 2,
+        mode:'horizontal',
+        loop: true
+    
+    });
+});
---
+++ left_nav.php
@@ -0,0 +1,15 @@
+<div id="left-nav">
+    <ul>
+        <li>
+        <a href="index.php" title="Home">Home</a>
+        </li>
+        <li>
+        <a href="footwear.php" title="Footwear">Footwear</a>
+        </li>                    
+        <li>
+        <a href="bags.php" title="Bags">Bags</a>
+        </li>                
+    </ul>
+    <p id="heading">United By Craft</p>
+</div><!-- #left-nav -->
+
